e4c790b162ed560c08736a73bfef870fc27f880b
ingfsm <ing.francosalasmedina@gmail.com>
2015-04-02 22:48:42 +0200

    trabajadas secciones de index.php

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+/* ==========================================================================
+  	Web Developers:
+   		Wendy Membreño Carranza
+   		Franco Salas Medina
+========================================================================== */
 
+$(function() {
+	/* Scroll Top */
+	$('.home').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+
+	/* Smooth scroll */
+	$('a.smooth').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	    var target = $(this.hash);
+	    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    if (target.length) {
+	        $('html,body').animate({
+	        	scrollTop: target.offset().top - parseInt(50)
+	        }, 1000);
+	        	return false;
+	    	}
+	    }
+	});
+});
